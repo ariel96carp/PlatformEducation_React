e4b801762b6967732d1c056e7ef07552a511e112
ariel96carp <88552083+ariel96carp@users.noreply.github.com>
2022-02-12 17:54:34 -0300

    Delete src/Components directory

--- src/Components/Routes/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-const ProtectedRoute = ({ children }) => {
-    const isAuth = localStorage.getItem("educationToken")
-    return isAuth ? children : <Navigate to="/login" />
-}
-
-export default ProtectedRoute
--- src/Components/Routes/PublicRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-const PublicRoute = ({ children }) => {
-    const isAuth = localStorage.getItem("educationToken")
-    return !isAuth ? children : <Navigate to="/" />
-}
-
-export default PublicRoute
